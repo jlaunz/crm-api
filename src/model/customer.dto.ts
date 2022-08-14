f4f1f2d200c2b4570bd1c3d149a557b3d359398f
John.Liao <john.liao@fisherfunds.co.nz>
2022-08-14 18:30:24 +1200

    CRM-000 Implement DTOs

--- src/model/customer.dto.ts
+++ src/model/customer.dto.ts
@@ -1,1 +1,125 @@
-export class CustomerDto{}
+import {
+    IsDate,
+    IsEmail,
+    IsEnum,
+    IsNotEmpty,
+    IsNumber,
+    IsOptional,
+    IsString,
+  } from 'class-validator';
+  import { Type } from 'class-transformer';
+  import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+  import { CustomerStatus } from '@prisma/client';
+  
+  export class CreateCustomerDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({ type: String, description: 'Customer first name' })
+    name: string;
+  
+    @IsEmail()
+    @IsNotEmpty()
+    @ApiProperty({ type: String, description: 'Customer first name' })
+    email: string;
+  
+  
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({ type: String, description: 'Customer address' })
+    addressLines: string;
+  
+    // ToDO: implement validation
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({ type: String, description: 'Customer post code' })
+    postcode: string;
+  
+    @IsEnum(CustomerStatus)
+    @IsNotEmpty()
+    @ApiProperty({
+      enum: CustomerStatus,
+      description: 'customer status.',
+    })
+    status: CustomerStatus;
+  }
+  
+  export class UpdateCustomerDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiPropertyOptional({ type: String, description: 'Customer first name' })
+    name: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiPropertyOptional({ type: String, description: 'Customer address' })
+    addressLines: string;
+  
+    // TO-DO: implement validation
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiPropertyOptional({ type: String, description: 'Customer post code' })
+    postcode: string;
+  
+    @IsEnum(CustomerStatus)
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiPropertyOptional({
+      enum: CustomerStatus,
+      description: 'customer status.',
+    })
+    status: CustomerStatus;
+  }
+  
+  export class QueryCustomerDto {
+    @IsString()
+    @IsOptional()
+    @ApiPropertyOptional({
+      type: String,
+      description:
+        'Find customers whose name contains provided string.',
+    })
+    name: string;
+  
+    @IsString()
+    @IsOptional()
+    @ApiPropertyOptional({
+      type: String,
+      description:
+        'Find customers by email',
+    })
+    email: string;
+  
+    @Type(() => Date)
+    @IsDate()
+    @IsOptional()
+    @ApiPropertyOptional({
+      type: String,
+      description: 'Format: yyyy-mm-dd. Find customers that are created after the provided date',
+    })
+    createdFrom: Date;
+  
+    @Type(() => Date)
+    @IsDate()
+    @IsOptional()
+    @ApiPropertyOptional({
+      type: String,
+      description: 'Format: yyyy-mm-dd. Find customers who are created before the provided date',
+    })
+    createdBefore: Date;
+  
+    @IsEnum(CustomerStatus)
+    @IsOptional()
+    @ApiPropertyOptional({ enum: CustomerStatus, description: 'customer status.' })
+    status: CustomerStatus;
+  }
+  
+  export class DeleteCustomerDto {
+    @IsNumber()
+    @IsNotEmpty()
+    @ApiPropertyOptional({ type: Number, description: 'Customer id' })
+    id: number;
+  }
+  
--- src/model/opportunity.dto.ts
+++ src/model/opportunity.dto.ts
@@ -1,1 +1,76 @@
-export class OpportunityDto{}
+import { OpportunityStatus } from 'prisma/prisma-client';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateOpportunityDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'Opportunity name' })
+  name: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @ApiProperty({ type: Number, description: 'Customer id' })
+  customerId: number;
+
+  @IsEnum(OpportunityStatus)
+  @IsNotEmpty()
+  @ApiProperty({
+    enum: OpportunityStatus,
+    description: 'opportunity status.',
+  })
+  status: OpportunityStatus;
+}
+
+export class QueryOpportunityDto {
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional({ type: String, description: 'opportunity name' })
+  name: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
+  @ApiPropertyOptional({ type: Number, description: 'customer id' })
+  customerId: number;
+
+  @IsEnum(OpportunityStatus)
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiPropertyOptional({
+    enum: OpportunityStatus,
+    description: 'opportunity status.',
+  })
+  status: OpportunityStatus;
+}
+
+export class UpdateOpportunityDto {
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional({ type: String, description: 'update opportunity name' })
+  name: string;
+
+  @IsEnum(OpportunityStatus)
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiPropertyOptional({
+    enum: OpportunityStatus,
+    description: 'opportunity status.',
+  })
+  status: OpportunityStatus;
+}
+
+export class DeleteOpportunityDto {
+  @Type(() => Number)
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiPropertyOptional({ type: Number, description: 'Opportunity id' })
+  id: number;
+}
