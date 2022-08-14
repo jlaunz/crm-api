3f5af260a7c8c673aac4645af7ffbc76027fdd25
John.Liao <john.liao@fisherfunds.co.nz>
2022-08-14 14:15:01 +1200

    CRM-000 Setup file structure

--- src/crm/crm.module.ts
+++ src/crm/crm.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CrmController } from './crm.controller';
+import { CustomerService } from './customer.service';
+import { OpportunityService } from './opportunity.service';
 
-@Module({})
+@Module({
+  controllers: [CrmController],
+  providers: [CustomerService, OpportunityService]
+})
 export class CrmModule {}
