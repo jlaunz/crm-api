ec8d6d7f8dfbe63dc964e27d14b0e392f68264e0
John.Liao <john.liao@fisherfunds.co.nz>
2022-08-14 14:05:51 +1200

    CRM-000 Setup Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CrmModule } from './crm/crm.module';
 
-
 @Module({
-  imports: [CrmModule],
+  imports: [
+    CrmModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
